eea9d5a1bf2859a3fc84caca65171343e52ce7dd
gerladeno <99estinY>
2019-06-28 19:19:30 +0300

    work in progress

--- temp.py
+++ temp.py
@@ -31,42 +31,42 @@ if __name__ == "__main__":
     addr = pd.DataFrame()
     xlss = []
     csvs = []
-    print(clean_rubbish("#____Trash -s-ds 0d-09d__) f-E(80-ru349r"))
-    print(clean_rubbish("# HID_PARTY"))
+    # print(clean_rubbish("#____Trash -s-ds 0d-09d__) f-E(80-ru349r"))
+    # print(clean_rubbish("# HID_PARTY"))
 
-#     for root, dirs, files in os.walk(path_to_project):
-#         xlss += [os.path.join(root, name) for name in files if
-#                  not root.__contains__('.hg') and root.__contains__('cdi-mats') and name.lower().__contains__(
-#                      '.xls') and not name.lower().__contains__('lock')]
-#
-#     for root, dirs, files in os.walk(path_to_project):
-#         csvs += [os.path.join(root, name) for name in files if
-#                  name.lower() == 'address.csv' and root.__contains__('cdi-mats')]
-#
-#     with open('xlss', 'w') as f:
-#         for i in xlss:
-#             f.write("%s\n" % i)
-#
-#     with open('csvs', 'w') as f:
-#         for i in csvs:
-#             f.write("%s\n" % i)
-#
-#     for name in csvs:
-#         df = read_csvs(name)
-#         for col in df.columns:
-#             col = clean_rubbish(col)
-#             if col not in addr.columns:
-#                 addr[col] = ""
-#         addr = pd.concat([addr, df], ignore_index=True, sort=False)
-#
-#     for name in xlss:
-#         df = read_xlss(name)
-#         for col in df.columns:
-#             col = clean_rubbish(col)
-#             if col not in addr.columns:
-#                 addr[col] = ""
-#
-#         addr = pd.concat([addr, df], ignore_index=True, sort=False)
-#
-#     addr = addr.drop_duplicates()
-#     addr.to_csv('addresses.csv', sep=';')
+    for root, dirs, files in os.walk(path_to_project):
+        xlss += [os.path.join(root, name) for name in files if
+                 not root.__contains__('.hg') and root.__contains__('cdi-mats') and name.lower().__contains__(
+                     '.xls') and not name.lower().__contains__('lock')]
+
+    for root, dirs, files in os.walk(path_to_project):
+        csvs += [os.path.join(root, name) for name in files if
+                 name.lower() == 'address.csv' and root.__contains__('cdi-mats')]
+
+    with open('xlss', 'w') as f:
+        for i in xlss:
+            f.write("%s\n" % i)
+
+    with open('csvs', 'w') as f:
+        for i in csvs:
+            f.write("%s\n" % i)
+
+    for name in csvs:
+        df = read_csvs(name)
+        for col in df.columns:
+            col_cleaned = clean_rubbish(col)
+            if col_cleaned not in addr.columns:
+                addr[col_cleaned] = ""
+        addr = pd.concat([addr, df], ignore_index=True, sort=False)
+
+    for name in xlss:
+        df = read_xlss(name)
+        for col in df.columns:
+            col = clean_rubbish(col)
+            if col not in addr.columns:
+                addr[col] = ""
+
+        addr = pd.concat([addr, df], ignore_index=True, sort=False)
+
+    addr = addr.drop_duplicates()
+    addr.to_csv('addresses.csv', sep=';')
